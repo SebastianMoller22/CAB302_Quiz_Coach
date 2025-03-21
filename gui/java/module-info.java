4222a5c21e81270fed76c990ad5b40da221314ed
Torsten Reibelt <n12090565@qut.edu.au>
2025-03-21 11:44:34 +1000

    Removed duplicate project code.

--- gui/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.addressbook {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.addressbook to javafx.fxml;
-    exports com.example.addressbook;
-}
