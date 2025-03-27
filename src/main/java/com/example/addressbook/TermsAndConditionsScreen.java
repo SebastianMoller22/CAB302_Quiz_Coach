4e8faa7c086bf9554322036ecaecd0380a8724f3
Torsten Reibelt <n12090565@qut.edu.au>
2025-03-27 18:34:25 +1000

    Renamed HelloController to TermsAndConditionsScreen

--- src/main/java/com/example/addressbook/TermsAndConditionsScreen.java
+++ src/main/java/com/example/addressbook/TermsAndConditionsScreen.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloController {
+public class TermsAndConditionsScreen {
     @FXML
     private Label welcomeText;
     @FXML
