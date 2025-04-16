53b81d96212c745fffc77b07b2afd3ae83439245
Le Quang Bui <n11571934@qut.edu.au>
2025-04-17 08:38:54 +1000

    Fix Login As User Method, Add Contructor, Add Signup Method

--- src/main/java/com/example/quizCoach/model/AuthenticationManager.java
+++ src/main/java/com/example/quizCoach/model/AuthenticationManager.java
@@ -3,12 +3,36 @@ package com.example.quizCoach.model;
 public class AuthenticationManager {
     private User activeuser;
 
+    public AuthenticationManager() {
+        activeuser = null;
+    }
+
     public User getActiveuser() {return activeuser;}
 
     public void setActiveuser(User user) {this.activeuser = user;}
 
     public Boolean validateString(String the_string, String regex) {
         return the_string.matches(regex);
+    }
+
+    public void Signup(String username, String name, String email, String password) throws Exception {
+        if (!validateString(username, AuthenticationConstant.usernameRegex)){
+            throw new Exception("Invalid Username");
+        }
+        if (checkifUserExists(username)) {
+            throw new Exception("This Username already existed");
+        }
+        if (!validateString(name, AuthenticationConstant.nameRegex)) {
+            throw new Exception("invalid Name");
+        }
+        if (!validateString(email, AuthenticationConstant.emailRegex)) {
+            throw new Exception("Invalid Email");
+        }
+        if (!validateString(password, AuthenticationConstant.passwordRegex)) {
+            throw new Exception("Invalid Password");
+        }
+        User newuser = new User(username, name, email, password);
+        // Insert into db
     }
 
     public Boolean checkifUserExists(String username) {
@@ -21,7 +45,7 @@ public class AuthenticationManager {
 
     public Boolean LoginAsUser(String username, String password) {
         if (matchPasswordandUsername(username, password)) {
-            activeuser = getUser(username);
+            setActiveuser(getUser(username));
             return true;
         } else {
             return false;
