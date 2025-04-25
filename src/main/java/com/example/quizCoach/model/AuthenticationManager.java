e2348d029ada3f7ec7ba8325d5e9a15ba9b895e3
Le Quang Bui <builequang5a3@gmail.com>
2025-04-26 01:29:08 +1000

    Comments added for AuthenticationManager class

--- src/main/java/com/example/quizCoach/model/AuthenticationManager.java
+++ src/main/java/com/example/quizCoach/model/AuthenticationManager.java
@@ -3,24 +3,58 @@ package com.example.quizCoach.model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * For Signup/Login/Logout and add/modify users' database
+ */
 public class AuthenticationManager {
+    /**
+     * the user using the session
+     */
     private User activeUser;
+
+    /**
+     * the users' database
+     */
     private SqliteUserDAO userDatabase;
 
+    /**
+     * the default constructor of the class
+     */
     public AuthenticationManager() {
         userDatabase = new SqliteUserDAO();
         activeUser = null;
     }
 
+    /**
+     * the method for getting the active user
+     * @return the current user of the session
+     */
     public User getActiveUser() {return activeUser;}
 
+    /**
+     * set the active user to an user
+     * @param user the user you want to set
+     */
     public void setActiveUser(User user) {this.activeUser = user;}
 
+    /**
+     * for validating users' inputs
+     * @param the_string the string you want to validate
+     * @param regex the regex you want to compare with
+     * @return whether the string is valid or not
+     */
     public Boolean validateString(String the_string, String regex) {
         return the_string.matches(regex);
     }
 
-    public void Signup(String username, String name, String email, String password) throws Exception {
+    /**
+     * for signing up as an user
+     * @param username a distinct username
+     * @param email the user's email
+     * @param password the user's password
+     * @throws Exception if any of the string is invalid
+     */
+    public void Signup(String username, String email, String password) throws Exception {
         if (!validateString(username, AuthenticationConstant.usernameRegex)){
             throw new Exception("Invalid Username");
         }
@@ -38,6 +72,11 @@ public class AuthenticationManager {
         userDatabase.addUser(newuser);
     }
 
+    /**
+     * Confirm whether the username has been used or not
+     * @param username the username you want to check
+     * @return whether the username has been used
+     */
     public Boolean checkifUserExists(String username) {
         List<User> users = userDatabase.getAllUsers();
         List<String> usernames = new ArrayList<>();
@@ -47,6 +86,13 @@ public class AuthenticationManager {
         return usernames.contains(username);
     }
 
+    /**
+     * to confirm whether the username and password match
+     * @param username the user's username
+     * @param password the user's password
+     * @return if the username and password match
+     * @throws Exception if they do not match
+     */
     public Boolean matchPasswordandUsername(String username, String password) throws Exception {
         User user = userDatabase.getUser(username);
         if (!checkifUserExists(username)) {
@@ -58,6 +104,13 @@ public class AuthenticationManager {
         return false;
     }
 
+    /**
+     * Used by the users to login to their accounts
+     * @param username the user's username
+     * @param password the user's password
+     * @return if the login complete
+     * @throws Exception if username and password do not match
+     */
     public Boolean LoginAsUser(String username, String password) throws Exception {
         if (matchPasswordandUsername(username, password)) {
             setActiveUser(getUser(username));
@@ -67,10 +120,18 @@ public class AuthenticationManager {
         }
     }
 
+    /**
+     * Log out of the account
+     */
     public void Logout() {
         setActiveUser(null);
     }
 
+    /**
+     * to get the user from the username
+     * @param username the username
+     * @return the user with the matching username
+     */
     public User getUser(String username) {
         // Return the user with that username
         return new User("Johnny", "aaBB1212@#@#", "hello@example.com");
