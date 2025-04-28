839158be1e9075f5adf0cbac9956e63069e30103
Sebastian <smolldragon22@gmail.com>
2025-04-28 17:54:34 +1000

    Added comments to Multiple_chose_maker and Quiz_maker

--- src/main/java/com/example/quizCoach/AI/Multiple_Choice_Maker.java
+++ src/main/java/com/example/quizCoach/AI/Multiple_Choice_Maker.java
@@ -10,10 +10,24 @@ import java.util.ArrayList;
 
 public class Multiple_Choice_Maker {
 
+    /*
+    The following are variables related to the multiple choice question
+    --------------------------------------------------------------------------------------------------------------------
+    Question: The question created by the AI
+    Answer: The answer to the Question
+    Options: list of Alternative WRONG options to the Question
+     */
     private String Question;
     private String Answer;
     private ArrayList<String> Options = new ArrayList<>();
 
+    /*
+    The following are Json related to the multiple choice question
+    --------------------------------------------------------------------------------------------------------------------
+    objectMapper: Object mapper that helps create all the following variables
+    jsonNode: The Json file node that will be used to store and create all the Json file with all the previous variables
+    Array: An array node of the alternative WORDING option, the same as Options
+     */
     private ObjectMapper objectMapper = new ObjectMapper();
     private ObjectNode jsonNode = objectMapper.createObjectNode();
     private ArrayNode Array = objectMapper.createArrayNode();
@@ -21,46 +35,82 @@ public class Multiple_Choice_Maker {
 
     public Multiple_Choice_Maker(String topic, int skill_level){
 
+        /*
+        Variables required to connect to the AI
+         */
         String apiURL = "http://127.0.0.1:11434/api/generate/";
         String model = "gemma3:12b";
+        /*
+        The prompt give to the AI to create the multiple choice question in a consistent formate
+        Takes in the topic, and skill level
+        NOTE: Skill level hasn't been implemented
+         */
         String prompt = String.format("Write a multiple Choice question about %s, with 4 options. The question should be scaled to a skill level of 1 where 1 is new to the topic and 100 is and expert. The question should be written in the format Q:A:O where 'Q' is the question, 'A' is the answer and 'O' are the other options, nothing else should be written.",topic);
 
+        /*
+        Collects AI answer
+         */
         OllamaResponseFetcher fetcher = new OllamaResponseFetcher(apiURL);
-
         OllamaResponse response = fetcher.fetchOllamaResponse(model, prompt);
 
+        /*
+        Serrated the AI answer by the end of line
+         */
         String[] Quetion = response.getResponse().split("\n");
 
-
+        /*
+        Removes the first 3 characters form each line
+         */
         for (int i = 0; i < Quetion.length; i++) {
             Quetion[i] = Quetion[i].substring(3);
         }
+
+        /*
+        Stores the Question and answer into the variables and jons node
+         */
         jsonNode.put("Question", Quetion[0]);
         this.Question = Quetion[0];
         jsonNode.put("Answer", Quetion[1]);
         this.Answer = Quetion[1];
 
-
+        /*
+        Stores the Alterative WORDING options into Options and array node
+         */
         for (int i = 2; i < Quetion.length; i++) {
             Array.add(Quetion[i]);
             this.Options.add(Quetion[i]);
         }
+        /*
+        Added the Array node to the json node
+         */
         jsonNode.put("Other_Options", Array);
 
     }
 
+    /*
+    When prompted gets the json node
+     */
     public ObjectNode getJsonNode() {
         return jsonNode;
     }
 
+    /*
+    When prompted gets the Answer
+     */
     public String getAnswer() {
         return Answer;
     }
 
+    /*
+    When prompted gets the Question
+     */
     public String getQuestion() {
         return Question;
     }
 
+    /*
+    When prompted gets the Options list
+     */
     public ArrayList<String> getOptions() {
         return Options;
     }
--- src/main/java/com/example/quizCoach/AI/Quiz_Maker.java
+++ src/main/java/com/example/quizCoach/AI/Quiz_Maker.java
@@ -13,12 +13,30 @@ import java.util.ArrayList;
 
 public class Quiz_Maker {
 
+    /*
+    The following are variables related to the quiz
+    --------------------------------------------------------------------------------------------------------------------
+    Topic: Is the inputted 'topic' that the AI will create the question about
+    Skill_level: Is the inputted 'skill level' that teh question the AI creates will scale the difficulty too
+    NumMultipleChoice: The inputted number of multiple choice questions to be created
+    NumShortResponse: The inputted number of short response questions to be created
+    MultipleChoiceArray: A created list of multiple chose question that can be easily accessed
+     */
     private String Topic;
     private int Skill_level;
     private int NumMultipleChoice;
     private int NumShortResponse;
 
     private ArrayList <Multiple_Choice_Maker> MultipleChoiceArray = new ArrayList<>();
+
+    /*
+    The following is the Json related variables
+    --------------------------------------------------------------------------------------------------------------------
+    objectMapper: Object mapper that helps create all the following variables
+    JsonNode: The Json file node that will be used to store and create all the Json file with all the previous varables
+    MultipleChoiceList: An array node of the multiple choice questions, the same as MultipleChoiceArray
+    ShortResponse: An array node of the short response questions, the same as (yet to be made)
+     */
 
     private ObjectMapper objectMapper = new ObjectMapper();
     private ObjectNode JsonNode = objectMapper.createObjectNode();
@@ -27,11 +45,18 @@ public class Quiz_Maker {
 
 
     public Quiz_Maker(String topic, int skill_level, int numMultipleChoice, int numShortResponse){
+        /*
+        Store all inputs into their respective variables
+         */
         this.Topic = topic;
         this.Skill_level = skill_level;
         this.NumMultipleChoice = numMultipleChoice;
         this.NumShortResponse = numShortResponse;
 
+        /*
+        Store the updated variables into the JsonNode
+        And create the questions and store them in Json node
+         */
         this.JsonNode.put("Topic", Topic);
         JsonNode.put("Number of MultipleChoice", NumMultipleChoice);
         JsonNode.put("Multiple Choice Questions", MakeMutipleChoice(Topic, Skill_level, NumMultipleChoice));
@@ -42,7 +67,11 @@ public class Quiz_Maker {
 
     private ArrayNode MakeMutipleChoice(String topic, int skill_level, int numMultipleChoice){
 
-
+        /*
+        Prompted the AI to crete a set number of multiple choice questions and store
+        them in MultipleChoiceArray and MultipleChoiceList.
+        Return MultipleChoiceList
+         */
         for (int j = 0; j < numMultipleChoice; j++) {
 
             Multiple_Choice_Maker Question = new Multiple_Choice_Maker(Topic, Skill_level);
@@ -55,14 +84,23 @@ public class Quiz_Maker {
 
 
     private ArrayNode MakeShortResponse(String topic, int skill_level, int numShortResponse){
-
+        /*
+        (yet to be made)
+         */
         return ShortResponse;
     }
 
+    /*
+    Get MultipleChoiceArray, so other classes can access the multiple choice questions
+     */
     public ArrayList<Multiple_Choice_Maker> getMultipleChoiceArray() {
         return MultipleChoiceArray;
     }
 
+
+    /*
+    create a Json file from the Json node and store it into the JSON folder for it to be uploaded
+     */
     public void UploadJSON(){
         try{
             objectMapper.writeValue(new File("src/main/java/com/example/quizCoach/JSON/Quiz.json"), JsonNode);
