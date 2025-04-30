e959d7b9040386eba5e0eccc429bbf666ef88283
Le Quang Bui <n11571934@qut.edu.au>
2025-05-01 02:45:22 +1000

    Fix AuthenticationConstant Class

--- src/main/java/com/example/quizCoach/model/AuthenticationConstant.java
+++ src/main/java/com/example/quizCoach/model/AuthenticationConstant.java
@@ -12,10 +12,10 @@ public class AuthenticationConstant {
     /**
      * the regex for checking the validity of email addresses
      */
-    public static final String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\\\.[a-zA-Z]{2,}$";
+    public static final String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
 
     /**
      * the regex for checking the validity of passwords
      */
-    public static final String passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\\\d)(?=.*[@$!%*?&])[A-Za-z\\\\d@$!%*?&]{8,}$";
+    public static final String passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
 }
