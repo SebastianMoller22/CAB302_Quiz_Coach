0accf62c6f6cd5a53c65e68329b7adc7df33d142
Le Quang Bui <n11571934@qut.edu.au>
2025-05-01 14:48:47 +1000

    Add Fields, MakeQuiz and shuffleQuestion method

--- src/main/java/com/example/quizCoach/model/QuizManager.java
+++ src/main/java/com/example/quizCoach/model/QuizManager.java
@@ -1,4 +1,32 @@
 package com.example.quizCoach.model;
 
+import com.example.quizCoach.AI.Quiz_Maker;
+
+import java.util.Random;
+
 public class QuizManager {
+    private Quiz activequiz;
+    private SqliteQuizDAO quizDatabase;
+
+    public QuizManager() {
+        quizDatabase = new SqliteQuizDAO();
+    }
+
+    public void MakeQuiz(String topic, int difficulty, int numMCQs) {
+        Quiz_Maker quizMaker = new Quiz_Maker(topic, difficulty, numMCQs, 0);
+        activequiz = quizMaker.get_quiz();
+    }
+
+    private void shuffleQuestion(Option[] options)
+    {
+        Random rnd = new Random();
+        for (int i = options.length - 1; i > 0; i--)
+        {
+            int index = rnd.nextInt(i + 1);
+            // Simple swap
+            Option a = options[index];
+            options[index] = options[i];
+            options[i] = a;
+        }
+    }
 }
