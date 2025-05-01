1e8e61804bda5d72f3268ee0d63efb380002bb36
Le Quang Bui <n11571934@qut.edu.au>
2025-05-01 16:49:05 +1000

    Complete loginButton in loginController

--- src/main/java/com/example/quizCoach/controller/loginController.java
+++ src/main/java/com/example/quizCoach/controller/loginController.java
@@ -1,5 +1,6 @@
 package com.example.quizCoach.controller;
 
+import com.example.quizCoach.model.AuthenticationManager;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -10,7 +11,7 @@ import javafx.scene.control.*;
 
 
 public class loginController {
-
+    @FXML private AuthenticationManager authentication;
     @FXML private TextField usernameField;
     @FXML private PasswordField passwordField;
     @FXML private Button loginButton;
@@ -23,15 +24,18 @@ public class loginController {
             String username = usernameField.getText();
             String password = passwordField.getText();
             // TODO: Add your login validation logic
-            System.out.println("Logging in: " + username);
 
             try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/quizCoach/home-page.fxml"));
-                Parent root = loader.load();
-                Stage stage = (Stage) loginButton.getScene().getWindow();
-                stage.setScene(new Scene(root, 800, 700));
+                if (authentication.LoginAsUser(username, password)) {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/quizCoach/home-page.fxml"));
+                    Parent root = loader.load();
+                    Stage stage = (Stage) loginButton.getScene().getWindow();
+                    stage.setScene(new Scene(root, 800, 700));
+                }
             } catch (IOException ex) {
                 ex.printStackTrace();
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
             }
         });
 
