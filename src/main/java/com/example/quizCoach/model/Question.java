8751e542351c2b1405799b4e20eaef7a8de03e87
Le Quang Bui <n11571934@qut.edu.au>
2025-05-01 17:21:04 +1000

    add shuffleQuestion method for Question

--- src/main/java/com/example/quizCoach/model/Question.java
+++ src/main/java/com/example/quizCoach/model/Question.java
@@ -1,5 +1,7 @@
 package com.example.quizCoach.model;
 
+import java.util.Random;
+
 public class Question {
     private String questionText;
     private Option[] options;
@@ -17,7 +19,21 @@ public class Question {
         return this.options;
     }
 
+    private void shuffleQuestion()
+    {
+        Random rnd = new Random();
+        for (int i = options.length - 1; i > 0; i--)
+        {
+            int index = rnd.nextInt(i + 1);
+            // Simple swap
+            Option a = options[index];
+            options[index] = options[i];
+            options[i] = a;
+        }
+    }
+
     public String[] GetOptionTexts() {
+        shuffleQuestion();
         String[] optiontexts = new String[options.length];
         for (int i = 0; i < options.length; i++) {
             optiontexts[i] = options[i].GetOptionText();
