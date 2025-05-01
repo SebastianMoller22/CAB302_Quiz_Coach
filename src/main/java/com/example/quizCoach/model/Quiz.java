2b39f942944da0d1c267afa1b634c8583382e8d4
Le Quang Bui <n11571934@qut.edu.au>
2025-05-01 13:47:36 +1000

    Remove allocated time Quiz Class

--- src/main/java/com/example/quizCoach/model/Quiz.java
+++ src/main/java/com/example/quizCoach/model/Quiz.java
@@ -5,12 +5,11 @@ public class Quiz {
     private String topic;
     private float difficulty;
     private Question[] questions;
-    private int allocatedTime;
 
-    public Quiz(float difficulty, Question[] questions, int allocatedTime) {
+    public Quiz(String topic, float difficulty, Question[] questions) {
+        this.topic = topic;
         this.difficulty = difficulty;
         this.questions = questions;
-        this.allocatedTime = allocatedTime;
     }
 
     public int GetQuizID() {
@@ -21,15 +20,13 @@ public class Quiz {
         this.quizID = quizID;
     }
 
+    public String GetTopic() {return topic;}
+
     public float GetDifficulty() {
         return difficulty;
     }
 
     public Question[] GetQuestions() {
         return questions;
-    }
-
-    public int GetAllocatedTime() {
-        return allocatedTime;
     }
 }
