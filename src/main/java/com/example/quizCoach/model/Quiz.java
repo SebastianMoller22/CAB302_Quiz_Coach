b14b9d20a5113875a7fb2d940f963c49cc59e8a3
Torsten Reibelt <n12090565@qut.edu.au>
2025-05-01 12:41:48 +1000

    Added Quiz, SqliteConnection, SqliteQuizDAO, and IQuizDAO back after they were removed during merge

---
+++ src/main/java/com/example/quizCoach/model/Quiz.java
@@ -0,0 +1,34 @@
+package com.example.quizCoach.model;
+
+public class Quiz {
+    private int quizID;
+    private float difficulty;
+    private Question[] questions;
+    private int allocatedTime;
+
+    public Quiz(float difficulty, Question[] questions, int allocatedTime) {
+        this.difficulty = difficulty;
+        this.questions = questions;
+        this.allocatedTime = allocatedTime;
+    }
+
+    public int GetQuizID() {
+        return quizID;
+    }
+
+    public void SetQuizID(int quizID) {
+        this.quizID = quizID;
+    }
+
+    public float GetDifficulty() {
+        return difficulty;
+    }
+
+    public Question[] GetQuestions() {
+        return questions;
+    }
+
+    public int GetAllocatedTime() {
+        return allocatedTime;
+    }
+}
