60a95e6a1e215ad3415a7e35f3bda12dc3d2cac4
Le Quang Bui <n11571934@qut.edu.au>
2025-05-04 05:34:16 +1000

    Implement QuizDB

--- src/main/java/com/example/quizCoach/model/QuizManager.java
+++ src/main/java/com/example/quizCoach/model/QuizManager.java
@@ -16,6 +16,7 @@ public class QuizManager {
     public void MakeQuiz(String topic, int difficulty, int numMCQs) {
         Quiz_Maker quizMaker = new Quiz_Maker(topic, difficulty, numMCQs, 0);
         activequiz = quizMaker.get_quiz();
+        quizDatabase.addQuiz(activequiz);
     }
 
     public Quiz getActivequiz() {return activequiz;}
