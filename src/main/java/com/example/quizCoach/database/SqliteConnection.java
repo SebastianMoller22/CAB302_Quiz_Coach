823ff3c15f8968b5394186a595d5341505bdeca2
Torsten Reibelt <n12090565@qut.edu.au>
2025-05-22 02:12:56 +1000

    Modified SqliteConnection class so that the jdbc url can be changed for testing

--- src/main/java/com/example/quizCoach/database/SqliteConnection.java
+++ src/main/java/com/example/quizCoach/database/SqliteConnection.java
@@ -4,21 +4,42 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SqliteConnection {
-    private static Connection instance = null;
+    private static String url = "jdbc:sqlite:quizCoach.db";
+    private static Connection conn;
 
-    private SqliteConnection() {
-        String url = "jdbc:sqlite:quizCoach.db";
-        try {
-            instance = DriverManager.getConnection(url);
-        } catch (SQLException sqlEx) {
-            System.err.println(sqlEx);
+    /**
+     * Used for overriding the Database url. Must be called before getInstance().
+     * @param jdbcUrl the url you want to override with
+     * */
+    public static synchronized void init(String jdbcUrl) {
+        url = jdbcUrl;
+        // if there is already connection, close it so it can be re-opened with the new URL
+        if (conn != null) {
+            try {
+                if (!conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException ignored) {
+            }
+            conn = null;
         }
     }
 
-    public static Connection getInstance() {
-        if (instance == null) {
-            new SqliteConnection();
+    /**
+     * Returns a Connection.  If no connection exists, or it's closed, opens a new one.
+     * @return Connection New connection to be return
+     */
+    public static synchronized Connection getInstance() throws SQLException {
+        if (conn == null || conn.isClosed()) {
+            conn = DriverManager.getConnection(url);
+            try {
+                conn.createStatement().execute("PRAGMA foreign_keys = ON;");
+            } catch (SQLException e) {
+                // ignore if not supported
+            }
         }
-        return instance;
+        return conn;
     }
+    // private constructor so instances of it can't be created because all its methods and state are static
+    private SqliteConnection() {}
 }
